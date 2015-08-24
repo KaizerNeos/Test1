ac5abfdd788a43dbf5e5e8eb06d20cfdbf25c325
Kin Seng <kaizer_92@hotmail.my>
2015-08-25 03:44:07 +0800

    AD done

---
+++ easyJet-Final/Controllers/BookTicketController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace easyJet_Final.Controllers
+{
+    public class BookTicketController : Controller
+    {
+        // GET: BookTicket
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ easyJet-Final/Controllers/SeatSelectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace easyJet_Final.Controllers
+{
+    public class SeatSelectionController : Controller
+    {
+        // GET: SeatSelection
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
